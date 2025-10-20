3546d98bc334c10cb9e7f1905e7a19e3508bc3af
Drakonkinst <drakonkinstmc@gmail.com>
2025-10-19 21:21:59 -0700

    Set up multi-page support

--- build/settings.js
+++ build/settings.js
@@ -1,4 +1,8 @@
-// Following https://eisenbergeffect.medium.com/an-esbuild-setup-for-typescript-3b24852479fe
+/**
+ * Sources:
+ * https://eisenbergeffect.medium.com/an-esbuild-setup-for-typescript-3b24852479fe
+ * https://how-to.dev/how-to-build-a-multipage-website-with-esbuild
+ */
 
 import esbuildPluginTsc from 'esbuild-plugin-tsc';
 
@@ -6,9 +10,14 @@ export const OUT_DIR = "public";
 
 export function createBuildSettings(options) {
     return {
-        entryPoints: ['src/main.ts'],
-        outfile: OUT_DIR + '/bundle.js',
+        entryPoints: [
+            'src/main.ts',
+            'src/overlay/overlay.ts',
+            'src/controller/controller.ts'],
+        outdir: OUT_DIR,
+        splitting: true,
         bundle: true,
+        format: "esm",
         plugins: [
             esbuildPluginTsc({
                 force: true
